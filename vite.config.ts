3e055bd1465aaada4f1a2d59dec25ed96868b9cb
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-11-27 12:48:55 -0800

    feat: @coinbase/build-onchain-apps (#96)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: './node_modules/.vite/@base-org/build-onchain-apps',
+  cacheDir: './node_modules/.vite/@coinbase/build-onchain-apps',
 
   plugins: [nxViteTsPaths()],
 
