26043f1687337f5f7424b4fc9f3fccbefd5ee365
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-14 13:51:39 -0800

    fix: tests

---
+++ apps/build-onchain-apps/jest.setup.ts
@@ -0,0 +1,14 @@
+// Jest is an amazing test runner and has some awesome assertion APIs 
+// built in by default. However, there are times when having more 
+// specific matchers (assertions) would be far more convenient.
+// https://jest-extended.jestcommunity.dev/docs/matchers/
+import 'jest-extended';
+// Enable jest-dom functions
+import '@testing-library/jest-dom';
+
+// WalletConnect has dependencies that do not run in the node environment.
+// To bypass this we'll mock the connector.
+jest.mock('@wagmi/core/connectors/walletConnect');
+
+
+jest.mock('wagmi');
---
+++ apps/build-onchain-apps/src/components/Header.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+jest.mock('wagmi');
+
+describe('Header', () => {
+  // Changes scroll state based on user scroll behavior
+  it('should change the scroll state based on user scroll behavior', () => {
+    // Mock the useState and useEffect hooks
+    const useStateMock = jest.spyOn(React, 'useState');
+    const useEffectMock = jest.spyOn(React, 'useEffect');
+
+    // Render the Header component
+    render(<Header />);
+
+    // Assert that the useState and useEffect hooks are called
+    expect(useStateMock).toHaveBeenCalledWith('at-top');
+    expect(useEffectMock).toHaveBeenCalled();
+  });
+});
