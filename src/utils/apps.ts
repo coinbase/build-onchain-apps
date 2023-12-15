60def62312a956ac71659b332c3b5d92232da8e7
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2023-12-15 16:22:17 -0500

    chore: Restructure contracts folder (#168)
    Co-authored-by: Sneh Koul <sneh.koul@coinbase.com>

--- src/utils/apps.ts
+++ src/utils/apps.ts
@@ -58,7 +58,9 @@ export const updatePackageJson = (
 export const displayFinalInstructions = (appName: string) => {
   console.log(chalk.green(`Onchain app '${appName}' created successfully! 🚀`));
   console.log(
-    chalk.blue(`Type 'cd apps/${appName}' to navigate into your new onchain app.\n`)
+    chalk.blue(
+      `Type 'cd apps/${appName}' to navigate into your new onchain app.\n`
+    )
   );
   console.log(chalk.blue(`Run 'yarn' to install dependencies.`));
   console.log(chalk.blue(`Run 'yarn dev' to start the development server.`));
