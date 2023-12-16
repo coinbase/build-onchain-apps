3243cd63645c8ea8b205f6a0c1d67d8e6752f933
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-15 23:59:35 -0800

    spaces

--- apps/build-onchain-apps/pages/buy-me-coffee/index.tsx
+++ apps/build-onchain-apps/pages/buy-me-coffee/index.tsx
@@ -11,12 +11,10 @@ export default function BuyMeCoffeePage() {
         description="Build Onchain Applications with the best consumer experience in a few minutes."
         image="themes.png"
       />
-
       <div>
         <Theme radius="medium" scaling="100%">
           <Header />
         </Theme>
-
         <Container mx={{ initial: '5', xs: '6', sm: '7', md: '9' }}>
           <Section size={{ initial: '2', md: '3' }}>
             <BuyMeCoffee />
