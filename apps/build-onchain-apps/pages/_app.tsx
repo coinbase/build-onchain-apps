bf07d4281b5825c5467cdc9ce446c394d3f2400e
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-18 16:32:16 -0800

    colors

--- apps/build-onchain-apps/pages/_app.tsx
+++ apps/build-onchain-apps/pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <OnchainProviders>
-        <Theme accentColor="orange">
+        <Theme>
           <MobileMenuProvider>
             <Component {...pageProps} />
           </MobileMenuProvider>
