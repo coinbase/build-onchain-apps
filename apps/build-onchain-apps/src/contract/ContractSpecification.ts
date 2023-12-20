c26a50884f2eaddb342bb7c706ebb1e15784fd67
Christopher Nascone <cnasc@users.noreply.github.com>
2023-12-20 16:36:40 -0500

    feat: basic multichain config (#186)

---
+++ apps/build-onchain-apps/src/contract/ContractSpecification.ts
@@ -0,0 +1,26 @@
+import { Abi, type Chain } from 'viem';
+import { baseGoerli } from 'viem/chains';
+import BuyMeACoffeeABI from './BuyMeACoffee';
+
+export type ContractInstance = {
+  chain: Chain;
+  address: `0x${string}`,
+  deactivated?: boolean,
+}
+
+export type ContractSpecification = Record<string, {
+  abi: Abi;
+  [chainId: number]: ContractInstance;
+}>;
+
+export const contracts = {
+  buyMeACoffee: {
+    abi: BuyMeACoffeeABI,
+    [baseGoerli.id]: {
+      chain: baseGoerli,
+      address: '0xeeC0bD3B58293ff45004C7eFf02917Beef28257c',
+    }
+    // more chains for this contract go here
+  }
+  // more contracts go here
+}
