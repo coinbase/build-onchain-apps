2e590a704a8aabec62917cc142a80cdd9410aff8
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-23 14:40:53 -0800

    fix

--- apps/build-onchain-apps/pages/home/index.tsx
+++ apps/build-onchain-apps/pages/home/index.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
             xmlnsXlink="http://www.w3.org/1999/xlink"
             viewBox="0 24 150 28"
             preserveAspectRatio="none"
-            shape-rendering="auto"
+            shapeRendering="auto"
           >
             <defs>
               <path
