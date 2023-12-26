49d0115610735470fca1a2defc22a9c44f4da465
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-26 14:23:13 -0800

    yo

--- apps/build-onchain-apps/tailwind.config.ts
+++ apps/build-onchain-apps/tailwind.config.ts
@@ -5,6 +5,12 @@ const config: Config = {
   theme: {
     container: {
       center: true,
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      }
     },
     extend: {
       backgroundImage: {
