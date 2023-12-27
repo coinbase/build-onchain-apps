c1d43f4e964c844f2c3e9969ef5053e82db26c99
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2023-12-27 11:21:47 -0800

    docs: added getting started

--- apps/build-onchain-apps/src/components/home/Guide.tsx
+++ apps/build-onchain-apps/src/components/home/Guide.tsx
@@ -1,12 +1,17 @@
-import { Flex } from '@radix-ui/themes';
 import CodeBlock from '../CodeBlock';
 
 export default function Guide() {
   return (
-    <Flex>
-      <CodeBlock>
-        <span>$</span> <span>npx @coinbase/build-onchain-apps@latest create</span>
-      </CodeBlock>
-    </Flex>
+    <div>
+      <div className="h-20">
+        <h3 className="text-4xl font-medium text-white mb-6">Getting started</h3>
+        <div className="h-px bg-white" />
+      </div>
+      <div>
+        <CodeBlock>
+          <span>$</span> <span>npx @coinbase/build-onchain-apps@latest create</span>
+        </CodeBlock>
+      </div>
+    </div>
   );
 }
