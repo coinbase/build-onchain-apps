f886554db7f78b2a873060ee59b9f706d8a2f384
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-03 22:54:53 -0800

    feat: fonts

--- apps/build-onchain-apps/pages/_document.tsx
+++ apps/build-onchain-apps/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function DocumentPage() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+        <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
