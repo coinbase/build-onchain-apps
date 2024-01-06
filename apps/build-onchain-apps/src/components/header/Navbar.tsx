5e8db371476f2a404bc7844f97bf732013935722
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-05 16:13:19 -0800

    ok

--- apps/build-onchain-apps/src/components/header/Navbar.tsx
+++ apps/build-onchain-apps/src/components/header/Navbar.tsx
@@ -4,7 +4,6 @@ import { forwardRef } from 'react';
 import { RemoveScroll } from 'react-remove-scroll';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import useActiveLink from '../../hooks/useActiveLink';
 
 export function NavbarLink({
   href,
@@ -15,13 +14,12 @@ export function NavbarLink({
   children: React.ReactNode;
   target?: string;
 }) {
-  const active = useActiveLink({ href });
   return (
     <NextLink
       href={href}
       className={clsx(
-        'px-[16px] py-[5px] text-center font-robotoMono text-base font-normal text-white',
-        active ? 'rounded-[50px] bg-neutral-900 bg-opacity-90' : '',
+        'px-[16px] py-[5px]',
+        'text-center font-robotoMono text-base font-normal text-white',
       )}
       target={target}
     >
@@ -43,13 +41,18 @@ const ListItem = forwardRef(function ListItem(
   ref: React.Ref<HTMLAnchorElement>,
 ) {
   return (
-    <li>
-      <NavigationMenu.Link asChild>
-        <a href={href} className={clsx('ListItemLink')} ref={ref} target={target}>
+    <div className="inline-flex items-center justify-start gap-8">
+      <NavigationMenu.Link asChild className="flex items-center justify-start gap-1">
+        <a
+          href={href}
+          className={clsx('text-center font-robotoMono text-base font-normal text-white')}
+          ref={ref}
+          target={target}
+        >
           {children}
         </a>
       </NavigationMenu.Link>
-    </li>
+    </div>
   );
 });
 
@@ -60,53 +63,33 @@ function Navbar({ isMenuOpen = false }: { isMenuOpen?: boolean }) {
         <NavbarLink href="https://github.com/coinbase/build-onchain-apps" target="_blank">
           <GitHubLogoIcon width="16" height="16" />
         </NavbarLink>
-        <NavbarLink href="/signature-mint">Signature Mint</NavbarLink>
-        <NavbarLink href="/buy-me-coffee">Buy My Coffee</NavbarLink>
-        <NavbarLink href="/mint">Mint</NavbarLink>
+        <NavbarLink href="/#get-started">Get Started</NavbarLink>
         <NavigationMenu.Root className="relative">
-          <NavigationMenu.List className="flex flex-row space-x-2 rounded-lg bg-white p-2 dark:bg-gray-800">
+          <NavigationMenu.List className={clsx('flex flex-row space-x-2 p-2')}>
             <NavigationMenu.Item>
               <NavigationMenu.Trigger
                 className={clsx(
-                  'rounded-md px-3 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-900',
-                  'text-sm font-medium',
-                  'text-gray-700 dark:text-gray-100',
-                  'focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75',
+                  'px-[16px] py-[5px]',
+                  'text-center font-robotoMono text-base font-normal text-white',
                 )}
               >
-                Learn
+                Experiences
               </NavigationMenu.Trigger>
               <NavigationMenu.Content
-                className={clsx('[animation-duration:250ms] [animation-timing-function:ease]')}
+                className={clsx(
+                  'inline-flex h-38 w-48 flex-col items-start justify-start gap-6',
+                  'rounded-lg bg-neutral-900 bg-opacity-90 p-6 shadow backdrop-blur-2xl',
+                )}
               >
-                <ul
-                  className={clsx(
-                    'm-0 grid grid-cols-[0.75fr_1fr] gap-x-[10px] p-[22px] [list-style:none]',
-                  )}
-                >
-                  <ListItem href="/buy-me-coffee">
-                    Buy My Coffee
-                  </ListItem>
-                </ul>
+                <ListItem href="/buy-me-coffee">Buy My Coffee</ListItem>
+                <ListItem href="/mint">Mint NFT</ListItem>
+                <ListItem href="/signature-mint">Signature Mint</ListItem>
               </NavigationMenu.Content>
             </NavigationMenu.Item>
-
-            <NavigationMenu.Indicator
-              className={clsx(
-                'z-10',
-                'top-[100%] flex h-2 items-end justify-center overflow-hidden',
-                'transition-[width_transform] duration-[250ms] ease-[ease]',
-              )}
-            >
-              <div className="relative top-1 h-2 w-2 rotate-45 bg-white dark:bg-gray-800" />
-            </NavigationMenu.Indicator>
           </NavigationMenu.List>
-
-          <div className={clsx('absolute flex justify-center', 'left-[-20%] top-[100%] w-[140%]')}>
-            <NavigationMenu.Viewport
-              className={clsx('relative mt-2 rounded-md bg-gray-800 shadow-lg')}
-            />
-          </div>
+          <NavigationMenu.Viewport
+            className={clsx('absolute flex justify-center', 'left-[-20%] top-[100%] w-[140%]')}
+          />
         </NavigationMenu.Root>
       </ul>
       {isMenuOpen && (
