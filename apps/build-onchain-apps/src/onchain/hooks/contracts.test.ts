b221cfe5a6887b828904260b0a8cde764238f877
Christopher Nascone <chris@nasc.one>
2024-01-08 16:17:10 -0500

    feat: generateContractHook coverage

--- apps/build-onchain-apps/src/onchain/hooks/contracts.test.ts
+++ apps/build-onchain-apps/src/onchain/hooks/contracts.test.ts
@@ -3,16 +3,17 @@
  */
 
 import { renderHook } from '@testing-library/react';
-import { baseGoerli } from 'viem/chains';
+import { baseGoerli, baseSepolia } from 'viem/chains';
+import { useNetwork } from 'wagmi';
 import BuyMeACoffeeABI from '../../contract/BuyMeACoffee';
 import { generateContractHook } from './contracts';
 
 jest.mock('wagmi', () => ({
   ...jest.requireActual<typeof import('wagmi')>('wagmi'),
-  useNetwork: jest.fn(() => ({ chain: {} })),
+  useNetwork: jest.fn(() => ({ chain: undefined })),
 }));
 
-//const mockUseNetwork = useNetwork as jest.MockedFunction<typeof useNetwork>;
+const mockUseNetwork = useNetwork as jest.MockedFunction<typeof useNetwork>;
 
 const useTestContract = generateContractHook({
   abi: BuyMeACoffeeABI,
@@ -20,17 +21,34 @@ const useTestContract = generateContractHook({
     chain: baseGoerli,
     address: '0xbasegoerli',
   },
+  [baseSepolia.id]: {
+    chain: baseSepolia,
+    address: '0xbasesepolia',
+    deactivated: true,
+  },
 });
 
 describe('generated contract hook', () => {
   beforeEach(() => {
-    //mockUseNetwork.mockReset();
+    mockUseNetwork.mockClear();
   });
-  it('handles when not connected', () => {
+  it.each([
+    ['notConnected', undefined, undefined],
+    ['onUnsupportedNetwork', { id: 31337 }, undefined],
+    ['deactivated', { id: baseSepolia.id }, undefined],
+    ['ready', { id: baseGoerli.id }, '0xbasegoerli'],
+  ])('handles %s state', (state, chain, address) => {
+    mockUseNetwork.mockImplementation(() => ({ chain: chain }) as ReturnType<typeof useNetwork>);
     const {
       result: { current },
     } = renderHook(() => useTestContract());
-    expect(current.status).toBe('onUnsupportedNetwork');
+    expect(current.status).toBe(state);
     expect(current.abi).toEqual(BuyMeACoffeeABI);
+    expect(current.supportedChains).toEqual([baseGoerli, baseSepolia]);
+    if (address) {
+      expect(current).toEqual(expect.objectContaining({ address }));
+    } else {
+      expect(current).not.toHaveProperty('address');
+    }
   });
 });
