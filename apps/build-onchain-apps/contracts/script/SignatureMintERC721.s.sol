91a297896d9fb68490df3668a47786601c6fee4f
Sneh Koul <sneh.koul@coinbase.com>
2024-01-08 18:46:40 -0500

    Optimize Contracts

--- apps/build-onchain-apps/contracts/script/SignatureMintERC721.s.sol
+++ apps/build-onchain-apps/contracts/script/SignatureMintERC721.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import {Script, console2} from "forge-std/Script.sol";
 import "../src/SignatureMintERC721.sol";
--- apps/build-onchain-apps/contracts/test/SignatureMintERC721.t.sol
+++ apps/build-onchain-apps/contracts/test/SignatureMintERC721.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import "../src/SignatureMintERC721.sol";
