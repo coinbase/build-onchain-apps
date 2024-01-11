61deafa08e88d96a5a6560548da9c79d2c1d7e42
Wes Pickett <wes.pickett@gmail.com>
2024-01-11 13:59:52 -0500

    docs in more places

--- apps/build-onchain-apps/app/page.tsx
+++ apps/build-onchain-apps/app/page.tsx
@@ -10,6 +10,7 @@ export const metadata = generateMetadata({
 
 /**
  * Server component, which imports the Home component (client component that has 'use client' in it)
+ * https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts
  * https://nextjs.org/docs/pages/building-your-application/upgrading/app-router-migration#step-4-migrating-pages
  * https://nextjs.org/docs/app/building-your-application/rendering/client-components
  */
