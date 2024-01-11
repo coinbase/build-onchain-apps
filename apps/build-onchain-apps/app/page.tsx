80a2fb6ffdb5fe237a574f937eb0ac608594aebc
Wes Pickett <wes.pickett@gmail.com>
2024-01-11 13:41:41 -0500

    chore: add a comment to explain the new app folder pattern

--- apps/build-onchain-apps/app/page.tsx
+++ apps/build-onchain-apps/app/page.tsx
@@ -8,6 +8,11 @@ export const metadata = generateMetadata({
   pathname: '',
 });
 
+/**
+ * Page components are a server component, which imports the Home component, which is a client component and has 'use client' at the top of its file.
+ * https://nextjs.org/docs/pages/building-your-application/upgrading/app-router-migration#step-4-migrating-pages
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components
+ */
 export default function Page() {
   return <Home />;
 }
