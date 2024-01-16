2215baa2478a32192aa8eede4a724a415272bd65
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-15 16:53:04 -0800

    fix: tailwind

--- apps/build-onchain-apps/tailwind.config.ts
+++ apps/build-onchain-apps/tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './onchainKit/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
