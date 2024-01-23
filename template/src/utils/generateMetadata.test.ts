15e83789a40ee1c88d467073b3ed80c38d6716d2
Wes Pickett <wes.pickett@gmail.com>
2024-01-23 17:14:47 -0500

    fix: formatting

--- template/src/utils/generateMetadata.test.ts
+++ template/src/utils/generateMetadata.test.ts
@@ -11,28 +11,29 @@ describe('generateMetadata', () => {
     expect(metadata.metadataBase).toEqual(new URL('http://localhost:3000'));
   });
 
-  const envs= [
+  const envs = [
     ['BOAT_DEPLOY_URL', 'boat-deploy-url.com', 'https://boat-deploy-url.com'],
-    ['VERCEL_URL', 'vercel-url.com', 'https://vercel-url.com']
+    ['VERCEL_URL', 'vercel-url.com', 'https://vercel-url.com'],
   ];
-  describe.each(envs)('generateMetadata with different environment variables', (envVar, envValue, expectedUrl) => {
-  
-    it(`should set metadataBase from ${envVar}`, async () => {
-      envs.forEach(([v]) => delete process.env[v]);
-      process.env[envVar] = envValue;
-      jest.resetModules();
-  
-      const { generateMetadata: generateMetadata2 } = await import('./generateMetadata');
-      const metadata = generateMetadata2({
-        title: 'Build Onchain Apps Toolkit',
-        description: 'Build Onchain Applications with the best consumer experience in a few minutes.',
-        images: 'themes.png',
-        pathname: '',
+  describe.each(envs)(
+    'generateMetadata with different environment variables',
+    (envVar, envValue, expectedUrl) => {
+      it(`should set metadataBase from ${envVar}`, async () => {
+        envs.forEach(([v]) => delete process.env[v]);
+        process.env[envVar] = envValue;
+        jest.resetModules();
+
+        const { generateMetadata: generateMetadata2 } = await import('./generateMetadata');
+        const metadata = generateMetadata2({
+          title: 'Build Onchain Apps Toolkit',
+          description:
+            'Build Onchain Applications with the best consumer experience in a few minutes.',
+          images: 'themes.png',
+          pathname: '',
+        });
+
+        expect(metadata.metadataBase).toEqual(new URL(expectedUrl));
       });
-  
-      expect(metadata.metadataBase).toEqual(new URL(expectedUrl));
-    });
-  
-  });
-  
+    },
+  );
 });
