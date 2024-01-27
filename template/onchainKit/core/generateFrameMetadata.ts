aa2e998e9d7b5d0aa796af9df794671cd6a3adcb
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-27 13:12:43 -0800

    frame

--- template/onchainKit/core/generateFrameMetadata.ts
+++
@@ -1,8 +0,0 @@
-export const generateFrameMetadata = function () {
-  return {
-    'fc:frame': 'vNext',
-    'fc:frame:button': 'boat',
-    'fc:frame:image': 'https://build-onchain-apps.vercel.app/release/v-0-17.png',
-    'fc:frame:post_url': 'post_url_test',
-  };
-};
