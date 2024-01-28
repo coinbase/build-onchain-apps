261bc259d808c22b43cada65af2629b181f2a4fc
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-27 16:07:59 -0800

    signer

--- template/app/api/frame/route.ts
+++ template/app/api/frame/route.ts
@@ -1,6 +1,56 @@
-import { NextResponse } from 'next/server';
+import { HubRpcClient, Message, getSSLHubRpcClient } from '@farcaster/hub-nodejs';
+import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
 
-async function getResponse(): Promise<NextResponse> {
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+const HUB_URL = 'nemes.farcaster.xyz:2283'; // URL of the Hub
+
+const client: HubRpcClient = getSSLHubRpcClient(HUB_URL);
+
+type FidResponse = {
+  verifications: string[];
+};
+
+async function getAddrByFid(fid: number) {
+  const options = {
+    method: 'GET',
+    url: `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
+    headers: { accept: 'application/json', api_key: 'NEYNAR_API_DOCS' },
+  };
+  const resp = await axios.get(options.url, { headers: options.headers });
+  if (resp.data?.users) {
+    const userVerifications = resp.data.users[0] as FidResponse;
+    if (userVerifications.verifications) {
+      return userVerifications.verifications[0];
+    }
+  }
+  return '0x00';
+}
+
+async function getFrameMessage(req: NextRequest): Promise<Message> {
+  const body: { trustedData?: { messageBytes?: string } } = await req.json();
+  const frameMessage: Message = Message.decode(
+    Buffer.from(body?.trustedData?.messageBytes ?? '', 'hex'),
+  );
+  return frameMessage;
+}
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let validatedMessage: Message | undefined = undefined;
+  let signer = '';
+  let fid = 0;
+  try {
+    const frameMessage: Message = await getFrameMessage(req);
+    const result = await client.validateMessage(frameMessage);
+    if (result.isOk() && result.value.valid && result.value.message) {
+      validatedMessage = result.value.message;
+    }
+    fid = validatedMessage?.data?.fid ?? 0;
+    signer = await getAddrByFid(fid);
+  } catch (err) {
+    console.error(err);
+  }
+
   return new NextResponse(`
     <!DOCTYPE html>
     <html>
@@ -9,7 +59,7 @@ async function getResponse(): Promise<NextResponse> {
         <meta name="fc:frame" content="vNext">
         <meta name="fc:frame:image" content="https://build-onchain-apps.vercel.app/release/v-0-17.png">
         <meta name="fc:frame:post_url" content="post_url_test">
-        <meta name="fc:frame:button:1" content="BOAT 2">
+        <meta name="fc:frame:button:1" content="${signer}">
       </head>
       <body>
         <p>BOAT Text</p>
@@ -18,8 +68,8 @@ async function getResponse(): Promise<NextResponse> {
   `);
 }
 
-export async function POST(): Promise<Response> {
-  return getResponse();
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
 }
 
 export const dynamic = 'force-dynamic';
