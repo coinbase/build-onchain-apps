53c0465fe4ddfdf77918d074f819685d69ea51a7
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-27 17:40:29 -0800

    test

--- template/app/api/frame/route.ts
+++ template/app/api/frame/route.ts
@@ -1,53 +1,12 @@
-import { HubRpcClient, Message, getSSLHubRpcClient } from '@farcaster/hub-nodejs';
+import { getFrameAccountAddress } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
- 
-type FidResponse = {
-  verifications: string[];
-};
-
-async function getFrameAccountAddress(body: { trustedData?: { messageBytes?: string } }) {
-  // URL of the Hub
-  const HUB_URL = 'nemes.farcaster.xyz:2283';
-  // Create a Hub RPC client
-  const client: HubRpcClient = getSSLHubRpcClient(HUB_URL);
-  let farcasterID = 0;
-  let validatedMessage: Message | undefined = undefined;
-  // Get the message from the request body
-  const frameMessage: Message = Message.decode(
-    Buffer.from(body?.trustedData?.messageBytes ?? '', 'hex'),
-  );
-  // Validate the message
-  const result = await client.validateMessage(frameMessage);
-  if (result.isOk() && result.value.valid && result.value.message) {
-    validatedMessage = result.value.message;
-  }
-  // Get the Farcaster ID from the message
-  farcasterID = validatedMessage?.data?.fid ?? 0;
-  // Get the user verifications from the Farcaster Indexer
-  const options = {
-    method: 'GET',
-    url: `https://api.neynar.com/v2/farcaster/user/bulk?fids=${farcasterID}`,
-    headers: { accept: 'application/json', api_key: 'NEYNAR_API_DOCS' },
-  };
-  const resp = await fetch(options.url, { headers: options.headers });
-  const responseBody = await resp.json();
-  // Get the user verifications from the response
-  if (responseBody.users) {
-    const userVerifications = responseBody.users[0] as FidResponse;
-    if (userVerifications.verifications) {
-      return userVerifications.verifications[0];
-    }
-  }
-  return '0x00';
-}
-
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let signer = '';
   try {
     const body: { trustedData?: { messageBytes?: string } } = await req.json();
-    signer = await getFrameAccountAddress(body);
+    signer = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
   } catch (err) {
     console.error(err);
   }
