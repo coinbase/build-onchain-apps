9c348473f71288f95a6dfcdc70302dab09ce5605
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-01-29 18:59:10 -0800

    chore: test latest kit (#300)

--- template/app/api/frame/route.ts
+++ template/app/api/frame/route.ts
@@ -1,11 +1,11 @@
-import { getFrameAccountAddress } from '@coinbase/onchainkit';
+import { FrameRequest, getFrameAccountAddress } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let signer: string | undefined = '';
   try {
-    const body: { trustedData?: { messageBytes?: string } } = await req.json();
+    const body: FrameRequest = await req.json();
     signer = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
   } catch (err) {
     console.error(err);
@@ -19,7 +19,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta name="fc:frame" content="vNext">
         <meta name="fc:frame:image" content="https://build-onchain-apps.vercel.app/release/v-0-17.png">
         <meta name="fc:frame:post_url" content="post_url_test">
-        <meta name="fc:frame:button:1" content="🌲 ${signer} 🌲">
+        <meta name="fc:frame:button:1" content="✨ ${signer} ✨">
       </head>
       <body>
         <p>BOAT Text</p>
