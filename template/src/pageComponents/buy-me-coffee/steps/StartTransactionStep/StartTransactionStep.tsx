cb2f9304dbc6162ec96911152a9f91259c9d186b
Arun Sood <arsood@gmail.com>
2024-01-29 23:36:51 -0800

    More content for start transaction

--- template/src/pageComponents/buy-me-coffee/steps/StartTransactionStep/StartTransactionStep.tsx
+++ template/src/pageComponents/buy-me-coffee/steps/StartTransactionStep/StartTransactionStep.tsx
@@ -1,7 +1,19 @@
 export default function StartTransactionStep() {
   return (
-    <h2 className="mb-5 w-full text-center text-2xl font-semibold text-white lg:text-left">
-      Coffee brewing...
-    </h2>
+    <>
+      <h2 className="mb-5 w-full text-center text-2xl font-semibold text-white">
+        Coffee brewing...
+      </h2>
+
+      <div className="text-center text-6xl">☕</div>
+
+      <div className="my-4 text-center text-gray-400">
+        Please confirm transaction in your wallet
+      </div>
+
+      <button type="submit" className="block w-full rounded-full bg-black py-4 text-center text-sm">
+        Transaction pending
+      </button>
+    </>
   );
 }
