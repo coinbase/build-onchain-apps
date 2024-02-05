4a7d1c58cf8dcd01ae3981f61799764c1457fa7d
Anirud Samala <anirud.samala+github@gmail.com>
2024-02-06 04:43:12 +0530

    chore: refactor, add tests (#321)

--- template/src/hooks/useOnchainCoffeeMemos.ts
+++ template/src/hooks/useOnchainCoffeeMemos.ts
@@ -1,9 +1,7 @@
-import { useCallback, useState } from 'react';
 import { useContractRead } from 'wagmi';
+import { CoffeeMemo } from '../types';
 import { markStep } from '../utils/analytics';
 import { useBuyMeACoffeeContract } from './contracts';
-
-import type { CoffeeMemo } from '../types';
 
 /**
  * Hooks is abstracting away the logic of calling a read-only function on a contract.
@@ -11,30 +9,21 @@ import type { CoffeeMemo } from '../types';
  * @returns The memos and a function to refetch them.
  */
 function useOnchainCoffeeMemos() {
-  // TODO: this state should be cleared on network change, otherwise memos from multiple chains
-  // could end up in the same list
-  const [memos, setMemos] = useState<CoffeeMemo[]>([]);
-
-  const handleSuccess = useCallback((newMemos: CoffeeMemo[]) => {
-    setMemos(newMemos);
-  }, []);
-
   const contract = useBuyMeACoffeeContract();
 
   // Below is a very basic example of how to call a read-only function on a contract (with no arguments).
   // The BuyMeACoffee-Contracts contract  https://github.com/alchemyplatform/RTW3-Week2-BuyMeACoffee-Contracts
   markStep('useContractRead.refetchMemos');
-  const { refetch: refetchMemos } = useContractRead({
+  const contractReadResult = useContractRead({
     address: contract.status === 'ready' ? contract.address : undefined,
     abi: contract.abi,
     functionName: 'getMemos',
-    onSuccess: handleSuccess,
   });
   markStep('useContractRead.refetchMemos');
 
   return {
-    memos,
-    refetchMemos,
+    memos: contractReadResult.status === 'success' ? (contractReadResult.data as CoffeeMemo[]) : [],
+    refetchMemos: contractReadResult.refetch,
   };
 }
 
