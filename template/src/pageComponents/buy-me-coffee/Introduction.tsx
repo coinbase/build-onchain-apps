066b2cbfed9fd7b80a51042556c0fa3489dc2a52
Piyush Lodaya <ppiyush13@gmail.com>
2024-02-05 12:18:00 +0530

    chore: Add todos, update image alt-text

--- template/src/pageComponents/buy-me-coffee/Introduction.tsx
+++ template/src/pageComponents/buy-me-coffee/Introduction.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import UserAvatar from './Avatar.svg';
 
 export default function Introduction() {
+  // TODO: Fetch these values dynamically
   const coffeeReceivedCount = 90;
   const userName = '0xPudgy';
   const aboutUser = 'I am a content creator producing videos about NFTs!';
@@ -10,12 +11,13 @@ export default function Introduction() {
     <section>
       <div className="mx-[-100vw] my-10 h-px bg-zinc-400 bg-opacity-20 md:my-16" />
       <div className="flex flex-col justify-start gap-8 md:flex-row md:items-center">
-        <Image alt="user-avatar" src={UserAvatar} className="h-28 w-28" />
+        {/* Fetch user avatar dynamically */}
+        <Image alt="User Avatar" src={UserAvatar} className="h-28 w-28" />
         <div className="flex flex-col items-start justify-start gap-2">
           <div className="w-96 text-3xl font-semibold text-white">{userName}</div>
           <div className="text-base font-normal leading-normal text-zinc-400">{aboutUser}</div>
           <div className="flex items-center justify-start gap-2">
-            <Image alt="coffee-icon" src="/coffee.svg" className="h-4 w-4" width={0} height={0} />
+            <Image alt="Coffees Received" src="/coffee.svg" className="h-4 w-4" width={0} height={0} />
             <div className="text-base font-normal leading-normal text-white">
               {coffeeReceivedCount} cups of coffee received
             </div>
