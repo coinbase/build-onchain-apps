74c5b4dfeb8eb04aca9200e13dd2fc7db3cb5c9f
Yuri <yuripetusko@gmail.com>
2024-02-12 04:12:36 +0100

    Estimate free 1155 mint fee and display them to user

--- template/src/pageComponents/mint/ContractDemo.tsx
+++ template/src/pageComponents/mint/ContractDemo.tsx
@@ -1,6 +1,8 @@
 import { useMemo, useState } from 'react';
-import { useAccount } from 'wagmi';
+import { encodeFunctionData, formatEther } from 'viem';
+import { useAccount, useEstimateGas } from 'wagmi';
 import { useCollectionMetadata } from '../../../onchainKit';
+import { SpinnerIcon } from '../../components/icons/SpinnerIcon';
 import NextImage from '../../components/NextImage/NextImage';
 import { EXPECTED_CHAIN } from '../../constants';
 import { useCustom1155Contract } from '../../hooks/contracts';
@@ -16,9 +18,9 @@ export enum MintSteps {
 }
 
 export default function MintContractDemo() {
-  const [mintStep, setMintStep] = useState<MintSteps | null>(null);
+  const [mintStep, setMintStep] = useState<MintSteps>(MintSteps.START_MINT_STEP);
 
-  const { chain } = useAccount();
+  const { chain, address } = useAccount();
 
   const contract = useCustom1155Contract();
 
@@ -29,6 +31,23 @@ export default function MintContractDemo() {
     contract.status === 'ready' ? contract.address : undefined,
     contract.abi,
   );
+
+  // Estimate free minting 1 erc1155 NFT
+  const { data: txFeeEstimation, isLoading: isLoadingFeeEstimate } = useEstimateGas({
+    to: contract.status === 'ready' ? contract.address : undefined,
+    account: address,
+    chainId: chain?.id,
+    data: address
+      ? encodeFunctionData({
+          abi: contract.abi,
+          functionName: 'mint',
+          args: [address, BigInt(1), BigInt(1), address],
+        })
+      : undefined,
+    query: { enabled: onCorrectNetwork && !!address },
+  });
+
+  const mintTxFeeEstimation = txFeeEstimation ? formatEther(txFeeEstimation, 'gwei') : undefined;
 
   const mintContent = useMemo(() => {
     return (
@@ -48,13 +67,13 @@ export default function MintContractDemo() {
     return <SwitchNetwork />;
   }
 
-  if (isLoading) {
-    // A future enhancement would be a nicer spinner here.
-    return <span className="text-xl">loading...</span>;
+  if (isLoading || isLoadingFeeEstimate) {
+    return (
+      <span className="text-xl">
+        <SpinnerIcon className="ml-r animate-spin" height="1.2rem" width="1.2rem" /> loading...
+      </span>
+    );
   }
-
-  // TODO: Retrieve this dynamically
-  const ethAmount = 0.0001;
 
   return (
     <div className="my-10 gap-16 lg:my-20 lg:flex">
@@ -68,7 +87,11 @@ export default function MintContractDemo() {
       <div className="flex-shrink-1 mt-10 w-full flex-grow-0 lg:mt-0">
         <h1 className="text-4xl font-bold">{collectionName}</h1>
 
-        <h2 className="my-5">{String(ethAmount)} ETH</h2>
+        {mintTxFeeEstimation && (
+          <h2 className="my-5">
+            Estimated tx fee: ~{mintTxFeeEstimation} {chain?.nativeCurrency.symbol ?? 'ETH'}
+          </h2>
+        )}
 
         <p className="mb-6 mt-4 text-sm text-boat-footer-light-gray">{description}</p>
 
