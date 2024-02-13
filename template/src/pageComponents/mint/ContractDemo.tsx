217abf435466e070e8df1c7d032a444efae73534
Yuri <yuripetusko@gmail.com>
2024-02-13 09:38:05 +0100

    Add loading spinner to Mint page. And don't hide everything while loading tx fee

--- template/src/pageComponents/mint/ContractDemo.tsx
+++ template/src/pageComponents/mint/ContractDemo.tsx
@@ -2,6 +2,7 @@ import { useMemo, useState } from 'react';
 import { encodeFunctionData, formatEther } from 'viem';
 import { useAccount, useEstimateGas } from 'wagmi';
 import { useCollectionMetadata } from '../../../onchainKit';
+import { SpinnerIcon } from '../../components/icons/SpinnerIcon';
 import NextImage from '../../components/NextImage/NextImage';
 import { EXPECTED_CHAIN } from '../../constants';
 import { useCustom1155Contract } from '../../hooks/contracts';
@@ -25,7 +26,12 @@ export default function MintContractDemo() {
 
   const onCorrectNetwork = chain?.id === EXPECTED_CHAIN.id;
 
-  const { collectionName, description, imageAddress, isLoading } = useCollectionMetadata(
+  const {
+    collectionName,
+    description,
+    imageAddress,
+    isLoading: isLoadingCollectionMetadata,
+  } = useCollectionMetadata(
     onCorrectNetwork,
     contract.status === 'ready' ? contract.address : undefined,
     contract.abi,
@@ -46,7 +52,7 @@ export default function MintContractDemo() {
     query: { enabled: onCorrectNetwork && !!address },
   });
 
-  const mintTxFeeEstimation = txFeeEstimation ? formatEther(txFeeEstimation, 'gwei') : undefined;
+  const mintTxFeeEstimation = txFeeEstimation ? formatEther(txFeeEstimation, 'gwei') : 'Unknown';
 
   const mintContent = useMemo(() => {
     return (
@@ -58,6 +64,8 @@ export default function MintContractDemo() {
     );
   }, [mintStep, collectionName]);
 
+  const isLoading = contract.status !== 'ready' || isLoadingCollectionMetadata;
+
   if (contract.status === 'notConnected') {
     return <NotConnected />;
   }
@@ -66,9 +74,14 @@ export default function MintContractDemo() {
     return <SwitchNetwork />;
   }
 
-  if (isLoading || isLoadingFeeEstimate) {
-    // A future enhancement would be a nicer spinner here.
-    return <span className="text-xl">loading...</span>;
+  if (isLoading) {
+    return (
+      <div className="my-5 flex justify-center align-middle">
+        <span className="text-xl">
+          <SpinnerIcon className="h-20 w-20 animate-spin" />
+        </span>
+      </div>
+    );
   }
 
   return (
@@ -83,11 +96,17 @@ export default function MintContractDemo() {
       <div className="flex-shrink-1 mt-10 w-full flex-grow-0 lg:mt-0">
         <h1 className="text-4xl font-bold">{collectionName}</h1>
 
-        {mintTxFeeEstimation && (
-          <h2 className="my-5">
-            Estimated tx fee: ~{mintTxFeeEstimation} {chain?.nativeCurrency.symbol ?? 'ETH'}
-          </h2>
-        )}
+        <h2 className="my-5">
+          Estimated tx fee:{' '}
+          {isLoadingFeeEstimate ? (
+            <SpinnerIcon className="inline animate-spin" height="1.2rem" width="1.2rem" />
+          ) : (
+            <>
+              {mintTxFeeEstimation}
+              {chain?.nativeCurrency.symbol ?? 'ETH'}
+            </>
+          )}
+        </h2>
 
         <p className="mb-6 mt-4 text-sm text-boat-footer-light-gray">{description}</p>
 
