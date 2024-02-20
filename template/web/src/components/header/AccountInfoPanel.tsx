c7cc9019647ddd8d93700611a3baec5af04d7258
Anirud Samala <anirud.samala+github@gmail.com>
2024-02-21 04:23:04 +0530

    feat: add chain, balance to account info

--- template/web/src/components/header/AccountInfoPanel.tsx
+++ template/web/src/components/header/AccountInfoPanel.tsx
@@ -1,11 +1,33 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Avatar, Name } from '@coinbase/onchainkit';
 import { ExitIcon } from '@radix-ui/react-icons';
+import { Address, Chain, formatEther } from 'viem';
 import { useAccount, useDisconnect } from 'wagmi';
 import { getSlicedAddress } from '@/utils/address';
+import { getRpcProviderForChain } from '@/utils/provider';
+
+const getBalance = async (address: Address, chain: Chain): Promise<bigint> => {
+  try {
+    return await getRpcProviderForChain(chain).getBalance({
+      address,
+    });
+  } catch (err) {
+    return 0n;
+  }
+};
 
 export function AccountInfoPanel() {
-  const { address } = useAccount();
+  const { address, chain } = useAccount();
+  const [balance, setBalance] = useState<bigint>(0n);
+
+  useEffect(() => {
+    if (address && chain) {
+      getBalance(address, chain)
+        .then((result) => setBalance(result))
+        .catch((error) => console.error(error));
+    }
+  }, [address, chain]);
+
   const { disconnect } = useDisconnect();
   const handleDisconnectWallet = useCallback(() => {
     disconnect();
@@ -25,6 +47,15 @@ export function AccountInfoPanel() {
             {getSlicedAddress(address)}
           </span>
         </div>
+      </div>
+      <hr className="h-px self-stretch border-transparent bg-zinc-400 bg-opacity-20" />
+      <div className="inline-flex flex-col py-4">
+        <span className="font-inter w-32 text-sm font-medium text-zinc-400">{chain?.name}</span>
+        <span className="font-inter w-32 text-base font-medium text-white">
+          {/* TODO: replace with OnchainKit component */}
+          {parseFloat(formatEther(balance)).toFixed(3)}
+          {' ETH'}
+        </span>
       </div>
       <hr className="h-px self-stretch border-transparent bg-zinc-400 bg-opacity-20" />
       <button
