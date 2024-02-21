a6a184153954a30cee9fe901bce66154384a4638
Anirud Samala <anirud.samala+github@gmail.com>
2024-02-22 00:37:12 +0530

    feat: update side nav feels (#405)

--- template/web/app/mint/_components/Guide.tsx
+++ template/web/app/mint/_components/Guide.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import CodeBlock from '@/components/code-block/CodeBlock';
 
 const codeStep1 = `\`\`\`solidity
@@ -13,18 +15,51 @@ modifier whenLive() {
 }`;
 
 export default function Guide() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    function convertRemToPixels(rem: number) {
+      return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    }
+
+    function handleScroll() {
+      const pageYOffset = window.scrollY;
+      let newActiveSectionId = null;
+
+      window.document.querySelectorAll('section[id]').forEach((section) => {
+        const sectionOffsetTop = (section as HTMLElement).offsetTop;
+
+        if (pageYOffset >= sectionOffsetTop - convertRemToPixels(7)) {
+          newActiveSectionId = section.id;
+        }
+      });
+
+      window.document
+        .querySelectorAll(`aside nav li a[href]`)
+        .forEach((linkItem) => linkItem.setAttribute('data-active', 'false'));
+
+      window.document
+        .querySelector(`nav li a[href="#${newActiveSectionId}"]`)
+        ?.setAttribute('data-active', 'true');
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <h3 className="mb-6 text-4xl font-medium text-white" id="guide">
         Guide
       </h3>
-      <div className="h-px bg-white" />
+      <hr className="h-px bg-white" />
       <div className="gap-16 lg:flex">
         <main className="w-full flex-shrink-0 flex-grow xl:max-w-[900px]">
-          <section className="mt-10 flex flex-col">
-            <h4 className="text-xl font-normal text-white" id="contract-summary">
-              Contract Summary
-            </h4>
+          <section className="mt-10 flex scroll-mt-28 flex-col" id="contract-summary">
+            <h4 className="text-xl font-normal text-white">Contract Summary</h4>
             <p className="my-4 text-base font-normal text-zinc-400">
               The <code>AllowlistNFT.sol</code> smart contract is an extension of an ERC721 smart
               contract. The{' '}
@@ -38,8 +73,8 @@ export default function Guide() {
               , and is optimized for gas savings and batch operations.
             </p>
           </section>
-          <section className="mt-10 flex flex-col">
-            <h4 className="text-xl font-normal text-white" id="publicMint-explanation">
+          <section className="mt-10 flex scroll-mt-28 flex-col" id="publicMint-explanation">
+            <h4 className="text-xl font-normal text-white">
               <code>publicMint</code> Explanation
             </h4>
             <p className="my-4 text-base font-normal text-zinc-400">
@@ -60,15 +95,27 @@ export default function Guide() {
             <CodeBlock code={codeStep2} language="solidity" />
           </section>
         </main>
-        <aside className="flex-shrink-1 mt-10 hidden w-full flex-grow-0 xl:block">
-          <nav>
-            <h2>On this page</h2>
-            <ul>
+        <aside className="flex-shrink-1 relative mt-10 hidden w-full flex-grow-0 xl:block">
+          <nav className="sticky top-28 flex flex-col gap-2 border-s-2 py-2 ps-4">
+            <h2 className="text-base font-bold">On this guide</h2>
+            <ul className="flex flex-col gap-2">
               <li>
-                <a href="/mint#contract-summary">Contract Summary</a>
+                <a
+                  href="#contract-summary"
+                  data-active={pathname.includes('#contract-summary')}
+                  className="text-base text-sm font-normal text-zinc-400 no-underline underline-offset-2 hover:underline data-[active=true]:text-white"
+                >
+                  Contract Summary
+                </a>
               </li>
               <li>
-                <a href="/mint#publicMint-explanation">publicMint explanation</a>
+                <a
+                  href="#publicMint-explanation"
+                  data-active={pathname.includes('#publicMint-explanation')}
+                  className="text-base text-sm font-normal text-zinc-400 no-underline underline-offset-2 hover:underline data-[active=true]:text-white"
+                >
+                  <code className="text-xs">publicMint</code> Explanation
+                </a>
               </li>
             </ul>
           </nav>
