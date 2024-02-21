c14449e0cc16f701b4922c9a787410dbfd617167
Arun Sood <arsood@gmail.com>
2024-02-21 14:28:25 -0800

    Add gold color

--- template/web/app/account-abstraction/_components/Vault.tsx
+++ template/web/app/account-abstraction/_components/Vault.tsx
@@ -1,7 +1,13 @@
 export default function Vault() {
   return (
-    <div className="w-full">
-      <h2 className="text-2xl">Vault</h2>
+    <div className="w-full px-8 pt-10">
+      <h2 className="mb-5 text-2xl">Vault</h2>
+
+      <h3 className="mb-1">Weapons</h3>
+      <div className="border-boat-gold border border-x-0 border-t-0" />
+
+      <h3 className="mb-1">Mystery boxes</h3>
+      <div className="border-boat-gold border border-x-0 border-t-0" />
     </div>
   );
 }
