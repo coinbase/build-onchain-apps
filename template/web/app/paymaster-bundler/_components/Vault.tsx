28ba7f48e999b04f6f16d84cab777243cb1d4435
Arun Sood <arsood@gmail.com>
2024-02-22 10:02:34 -0800

    Roboto font

--- template/web/app/paymaster-bundler/_components/Vault.tsx
+++ template/web/app/paymaster-bundler/_components/Vault.tsx
@@ -5,7 +5,7 @@ export default function Vault() {
     <div className="w-full rounded-bl-xl bg-boat-color-gray-900 px-8 py-10">
       <h2 className="font-roboto mb-5 text-2xl">Vault</h2>
 
-      <h3 className="mb-1">Weapons</h3>
+      <h3 className="font-roboto mb-1 text-lg">Weapons</h3>
       <div className="border-boat-gold border border-x-0 border-t-0" />
 
       <div className="my-4 flex gap-4">
@@ -39,7 +39,7 @@ export default function Vault() {
         </div>
       </div>
 
-      <h3 className="mb-1">Mystery boxes</h3>
+      <h3 className="font-roboto mb-1 text-lg">Mystery boxes</h3>
       <div className="border-boat-gold border border-x-0 border-t-0" />
 
       <div className="my-4 flex gap-4">
