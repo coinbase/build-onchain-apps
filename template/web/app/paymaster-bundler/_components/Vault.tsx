a99abcd06dbb53eb49d73037d28fac18f89fdb73
Arun Sood <arsood@gmail.com>
2024-02-22 09:58:50 -0800

    Roboto Mono font

--- template/web/app/paymaster-bundler/_components/Vault.tsx
+++ template/web/app/paymaster-bundler/_components/Vault.tsx
@@ -3,10 +3,10 @@ import NextImage from '@/components/NextImage/NextImage';
 export default function Vault() {
   return (
     <div className="w-full rounded-bl-xl bg-boat-color-gray-900 px-8 py-10">
-      <h2 className="mb-5 text-2xl">Vault</h2>
+      <h2 className="font-roboto mb-5 text-2xl">Vault</h2>
 
       <h3 className="mb-1">Weapons</h3>
-      <div className="border border-x-0 border-t-0 border-boat-gold" />
+      <div className="border-boat-gold border border-x-0 border-t-0" />
 
       <div className="my-4 flex gap-4">
         <div className="w-full">
@@ -40,7 +40,7 @@ export default function Vault() {
       </div>
 
       <h3 className="mb-1">Mystery boxes</h3>
-      <div className="border border-x-0 border-t-0 border-boat-gold" />
+      <div className="border-boat-gold border border-x-0 border-t-0" />
 
       <div className="my-4 flex gap-4">
         <div className="w-full">
