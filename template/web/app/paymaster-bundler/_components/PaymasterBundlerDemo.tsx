cadd886ac84c77075fde0a53046c3735fece5f42
Arun Sood <arsood@gmail.com>
2024-02-23 11:58:55 -0800

    Refactor constants

--- template/web/app/paymaster-bundler/_components/PaymasterBundlerDemo.tsx
+++ template/web/app/paymaster-bundler/_components/PaymasterBundlerDemo.tsx
@@ -11,19 +11,12 @@ import { createPimlicoPaymasterClient } from 'permissionless/clients/pimlico';
 import { PublicClient, WalletClient, createWalletClient, custom } from 'viem';
 import { createPublicClient, http } from 'viem';
 import { sepolia } from 'viem/chains'; // Replace this with the chain used by your application
+import { rpcUrl, paymasterUrl, entryPoint, factoryAddress } from '../constants';
 import { nftAbi } from './abi';
 
 import GamePlay from './GamePlay';
 import Header from './Header';
 import Vault from './Vault';
-
-// constants
-const rpcUrl =
-  'https://api.developer.coinbase.com/rpc/v1/base-sepolia/Nz9nSdr8uIY7HhIi2zRrJFgISslFF2In';
-const paymasterUrl =
-  'https://api.developer.coinbase.com/rpc/v1/base-sepolia/Nz9nSdr8uIY7HhIi2zRrJFgISslFF2In';
-const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
-const factoryAddress = '0x9406Cc6185a346906296840746125a0E44976454';
 
 export default function PaymasterBundlerDemo() {
   const { logout } = usePrivy();
