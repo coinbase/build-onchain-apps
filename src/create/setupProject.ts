330337b4dbfc812140ed0e5cf3622ba6d91438f3
Ky Lee <132851666+kyhyco@users.noreply.github.com>
2024-02-22 23:23:52 -0800

    fix: cleaner blank app (#425)

--- src/create/setupProject.ts
+++ src/create/setupProject.ts
@@ -43,16 +43,7 @@ async function execAsync(command: string, options = {}) {
 function setupBlankApp(projectDir: string) {
   const appPath = path.join(projectDir, './web/app/page.tsx');
 
-  const content = `import { generateMetadata } from '@/utils/generateMetadata';
-
-export const metadata = generateMetadata({
-  title: 'Build Onchain Apps Toolkit',
-  description: 'Build Onchain Applications with the best consumer experience in a few minutes.',
-  images: 'themes.png',
-  pathname: '',
-});
-
-/**
+  const content = `/**
  * Server component, which imports the Home component (client component that has 'use client' in it)
  * https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts
  * https://nextjs.org/docs/pages/building-your-application/upgrading/app-router-migration#step-4-migrating-pages
