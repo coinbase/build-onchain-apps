24579d325ab68b865b253f1dfac2d260bf65a4a6
Sneh1999 <snehkoul1999@gmail.com>
2024-02-24 01:41:09 +0530

    remose blank setup

--- src/create/setupProject.ts
+++ src/create/setupProject.ts
@@ -45,22 +45,6 @@ async function execAsync(command: string, options = {}) {
   });
 }
 
-function setupBlankApp(projectDir: string) {
-  const appPath = path.join(projectDir, './web/app/page.tsx');
-
-  const content = `/**
- * Server component, which imports the Home component (client component that has 'use client' in it)
- * https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts
- * https://nextjs.org/docs/pages/building-your-application/upgrading/app-router-migration#step-4-migrating-pages
- * https://nextjs.org/docs/app/building-your-application/rendering/client-components
- */
-export default function Page() {
-  return <div>Hello World</div>;
-}
-`;
-  fs.writeFileSync(appPath, content);
-}
-
 export async function setupProject(projectDir: string, project) {
   try {
     const spinner = prompts.spinner();
