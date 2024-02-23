37017d005ed7d6ae953d297828cbc49b589f4ad6
Ky Lee <hello@kyhy.co>
2024-02-22 19:09:48 -0800

    refactor guide

--- template/web/app/home/_components/WhyUseIt.tsx
+++ template/web/app/home/_components/WhyUseIt.tsx
@@ -2,9 +2,9 @@ import { CheckIcon } from '@radix-ui/react-icons';
 
 export default function HomeMain() {
   return (
-    <section className="mb-24 flex flex-col items-center justify-center">
+    <section className="mb-12 flex flex-col items-center justify-center">
       <div className="w-full md:w-4/5">
-        <h2 className="mb-14 text-center text-xl font-medium text-white md:text-2xl lg:text-3xl">
+        <h2 className="mb-10 text-center text-xl font-medium text-white md:text-2xl lg:text-3xl">
           Save weeks of initial app setup and the hassle of integrating onchain components with web2
           infrastructure.
         </h2>
