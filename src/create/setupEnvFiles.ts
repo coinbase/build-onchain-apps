18c725a6cd4e6807b32f7eb318ad09d563f5a4ae
Ky Lee <132851666+kyhyco@users.noreply.github.com>
2024-02-26 11:59:31 -0800

    chore: pre release (#446)

--- src/create/setupEnvFiles.ts
+++ src/create/setupEnvFiles.ts
@@ -6,15 +6,12 @@ export function setupEnvFiles(projectDir: string, envVar: EnvVar) {
   if (envVar === undefined) return;
 
   const envLocalPath = path.join(projectDir, './web/.env.local');
-  const content = `
-  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID=
-  NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID=${
-    envVar.walletConnectProjectID ?? 'NOT_CONFIGURED'
-  }
-  NEXT_PUBLIC_RPC_URL="GET_FROM_COINBASE_CLOUD"
-  NEXT_PUBLIC_PAYMASTER_URL="GET_FROM_COINBASE_CLOUD"
-  NEXT_PUBLIC_PRIVY_ID="GET_FROM_PRIVY"
-  ENVIRONMENT=localhost
+  const content = `NEXT_PUBLIC_GOOGLE_ANALYTICS_ID=
+NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID=${envVar.walletConnectProjectID ?? '"GET_ID_FROM_WALLET_CONNET" # See https://cloud.walletconnect.com'}
+NEXT_PUBLIC_RPC_URL="GET_FROM_COINBASE_CLOUD"
+NEXT_PUBLIC_PAYMASTER_URL="GET_FROM_COINBASE_CLOUD"
+NEXT_PUBLIC_PRIVY_ID="GET_FROM_PRIVY"
+ENVIRONMENT=localhost
 `;
   fs.writeFileSync(envLocalPath, content);
 
