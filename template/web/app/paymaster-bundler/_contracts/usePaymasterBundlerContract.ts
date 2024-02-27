f54342f63bf2fb5eed9d40b09bd68c791fd8528f
Arun Sood <arsood@gmail.com>
2024-02-27 13:27:06 -0800

    Add hook for Paymaster Bundler contract

---
+++ template/web/app/paymaster-bundler/_contracts/usePaymasterBundlerContract.ts
@@ -0,0 +1,17 @@
+import { baseSepolia } from 'viem/chains';
+import { generateContractHook } from '@/hooks/contracts';
+import { entryPoint } from '../constants';
+import { PaymasterBundlerABI } from './PaymasterBundlerABI';
+
+/**
+ * Returns contract data for the PaymasterBundler contract.
+ */
+export const usePaymasterBundlerContract = generateContractHook({
+  abi: PaymasterBundlerABI,
+  [baseSepolia.id]: {
+    chain: baseSepolia,
+    address: entryPoint,
+  },
+
+  // ... more chains for this contract go here
+});
