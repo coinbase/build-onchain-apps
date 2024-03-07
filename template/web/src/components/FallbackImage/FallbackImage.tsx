d336e479b67f772c915ef01ef593d7aef619b55e
Yuri <yuripetusko@gmail.com>
2024-03-07 17:53:41 +0100

    Remove css module with css clamp, as 50% achieves the same result for these needs

--- template/web/src/components/FallbackImage/FallbackImage.tsx
+++ template/web/src/components/FallbackImage/FallbackImage.tsx
@@ -1,16 +1,9 @@
 import { ImageIcon } from '@radix-ui/react-icons';
-import { clsx } from 'clsx';
-import styles from './FallbackImage.module.css';
 
 export function FallbackImage() {
   return (
-    <div className="flex size-full items-center justify-center bg-slate-100">
-      <ImageIcon
-        className={clsx(
-          styles.FallbackImageIcon,
-          'font-robotoMono fill-slate-400 stroke-slate-400',
-        )}
-      />
+    <div className="flex size-full items-center justify-center bg-slate-100 ">
+      <ImageIcon className="font-robotoMono h-6/12 w-6/12 fill-slate-400 stroke-slate-400" />
     </div>
   );
 }
