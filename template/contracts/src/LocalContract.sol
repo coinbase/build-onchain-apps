5d1c0015d42e59247b6c0d60f5dd2daba3eb457e
Ky Lee <hello@kyhy.co>
2024-04-17 10:59:37 -0700

    update pragma solidity

--- template/contracts/src/LocalContract.sol
+++ template/contracts/src/LocalContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Script, console2} from "forge-std/Script.sol";
--- template/contracts/test/AllowlistNFT.t.sol
+++ template/contracts/test/AllowlistNFT.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import "forge-std/Test.sol";
 import {AllowlistNFT} from "../src/AllowlistNFT.sol";
