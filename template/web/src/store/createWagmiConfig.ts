8bf54d0cbfae09c4c385073d27c444c425dfd9cb
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-04-30 13:51:44 -0700

    test

--- template/web/src/store/createWagmiConfig.ts
+++ template/web/src/store/createWagmiConfig.ts
@@ -2,7 +2,7 @@ import { createConfig, http } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 
-export function createWagmiConfig(projectId: string) {
+export function createWagmiConfig(projectId?: string) {
   // Keep this till we fully deprecated RK inside the template
   if (projectId) {
     console.log('projectId:', projectId);
