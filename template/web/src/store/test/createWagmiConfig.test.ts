7c3df5df537dc10bd56fc1960208bb349e169077
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-04-30 17:47:34 -0400

    fix

--- template/web/src/store/test/createWagmiConfig.test.ts
+++ template/web/src/store/test/createWagmiConfig.test.ts
@@ -22,7 +22,7 @@ describe('createWagmiConfig', () => {
     Environment.production,
   ])('returns valid Wagmi config when env=%s', (environment) => {
     process.env[EnvironmentKeys.environment] = environment;
-    const config = createWagmiConfig();
+    const config = createWagmiConfig('https://api.developer.coinbase.com/rpc/v1/base/example');
     expect(config.chains[0].id).toEqual(baseSepolia.id);
   });
 });
