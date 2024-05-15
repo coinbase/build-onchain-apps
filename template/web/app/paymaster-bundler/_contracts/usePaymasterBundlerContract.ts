2b03cc6675a07ff6235df2562a91a629d634d9ee
Paul Cramer <paulccramer@gmail.com>
2024-05-15 13:11:46 -0400

    Remove comments

--- template/web/app/paymaster-bundler/_contracts/usePaymasterBundlerContract.ts
+++ template/web/app/paymaster-bundler/_contracts/usePaymasterBundlerContract.ts
@@ -3,10 +3,7 @@ import { baseSepolia } from 'viem/chains';
 import { generateContractHook } from '@/hooks/contracts';
 
 export const usePaymasterBundlerContract = generateContractHook({
-    // address: '0x119Ea671030FBf79AB93b436D2E20af6ea469a19',
     abi: myNFTABI,
-    // supported: 'safeMint',
-    // to: account.address,
     [baseSepolia.id]: {
         chain: baseSepolia,
         address: '0x119Ea671030FBf79AB93b436D2E20af6ea469a19',
