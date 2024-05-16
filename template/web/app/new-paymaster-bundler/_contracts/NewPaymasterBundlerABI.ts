e88829dc4259f3c580a6d017e1ce4b2da58ad0c9
Paul Cramer <paulccramer@gmail.com>
2024-05-16 13:50:20 -0400

    Add Coinbase Smart Wallet ABIs

---
+++ template/web/app/new-paymaster-bundler/_contracts/NewPaymasterBundlerABI.ts
@@ -0,0 +1,9 @@
+export const abi = [
+  {
+    stateMutability: 'nonpayable',
+    type: 'function',
+    inputs: [{ name: 'to', type: 'address' }],
+    name: 'safeMint',
+    outputs: [],
+  },
+] as const;
