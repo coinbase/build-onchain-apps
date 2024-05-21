1eeea7ea591b38cb9f9573a6a9cc0d4958d1fc67
Paul Cramer <paulccramer@gmail.com>
2024-05-21 19:35:43 -0400

    fix import

--- template/web/src/components/SmartWallets/PaymasterClient.ts
+++ template/web/src/components/SmartWallets/PaymasterClient.ts
@@ -1,7 +1,6 @@
 import { ENTRYPOINT_ADDRESS_V06 } from 'permissionless';
 import { paymasterActionsEip7677 } from 'permissionless/experimental';
-import { createClient, http } from 'viem';
-import { createPublicClient } from 'viem';
+import { createPublicClient, createClient, http } from 'viem';
 import { baseSepolia } from 'viem/chains';
 
 const paymasterService = process.env.NEXT_PUBLIC_PAYMASTER_URL ?? '';
